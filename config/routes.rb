4f7dd9cff6587f0894d23d7d2caf9a6e0cf26347
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-05 14:48:00 +0300

    Describe the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
-
-  get 'user/index'
-  get 'user/new'
-  get 'user/show'
-  get 'user/create'
 
   resources :users, only: [:index, :show, :create, :new] do
     resources :posts, only: [:index, :show, :create, :new]
