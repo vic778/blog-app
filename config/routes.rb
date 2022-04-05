27c97aa68dbd84a7a693e5e10e6308383e844df6
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-05 13:51:44 +0300

    Add route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :post
-  # get 'post/index'
-  # get 'post/new'
-  # get 'post/show'
-  # get 'post/create'
+  resources :posts
 
   get 'user/index'
   get 'user/new'
   get 'user/show'
   get 'user/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show, :create, :new] do
+    resources :posts, only: [:index, :show, :create, :new]
+  end
+
 end
