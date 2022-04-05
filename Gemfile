8c1a34bd882a4b82a4307735030e330b03c82e8d
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-05 12:29:09 +0300

    Create a post model

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem "better_errors"
 end
 
 group :test do
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :post
+  # get 'post/index'
+  # get 'post/new'
+  # get 'post/show'
+  # get 'post/create'
+
+  get 'user/index'
+  get 'user/new'
+  get 'user/show'
+  get 'user/create'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
