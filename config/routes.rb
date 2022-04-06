1c3ad28832d339fc7cf19b1408c80222426df6bb
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-06 15:44:55 +0300

    Associated models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'comments/index'
 
     root "users#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_094910) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_124225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.integer "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "commets_counter"
+    t.integer "likes_counter"
   end
 
   create_table "users", force: :cascade do |t|
