be9f62963220741532a30d1ab90c46bca5cc4d7f
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-12 18:31:33 +0300

    Add for loop to display all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,27 +3,14 @@ class PostsController < ApplicationController
     @user = User.includes(:posts).find(params[:user_id])
   end
 
-  def new
-    @post = Post.new
-  end
-
   def show
     @user = @user = User.find(params[:user_id])
     @post = @user.posts.includes(:comments).find(params[:id])
   end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
-
   private
 
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:author, :title, :text)
   end
 end
