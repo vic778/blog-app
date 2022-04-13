20d60e9cf6e859c5a2dd3f26d6dc97f27c5e343c
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-13 20:03:38 +0300

    Make changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'comments/index'
+
 
     root "users#index"
 
     resources :users, only: [:index, :show] do
         resources :posts, only: [:index, :show, :create, :new]
     end
+
+    resources :posts do
+      resources :comments, only: %i[create destroy]
+        resources :likes, only: %i[create]
+  end
 
 end
