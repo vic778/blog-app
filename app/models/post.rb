4b2266e9c3a0dbe6349d49f323d094323ef6b447
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-14 15:53:20 +0300

    Make required the title and bio

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
+  after_save :update_post_counter
+
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  after_save :update_post_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.limit(5).order(created_at: :desc)
