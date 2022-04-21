c034f408887d0518231dc3b8e4a49ae35111fcdb
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-21 09:42:40 +0300

    Create route for Api

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,12 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: %i[index] do
+        resources :comments, only: %i[index create]
+      end
+    end
+  end
+
 end
